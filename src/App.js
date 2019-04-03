f8cb68cba56e4ef0b9bc05f9a1e0e3a055ac61ea
dzenvikas <vikaskumar.vsk@gmail.com>
2019-04-04 01:51:17 +0530

    components, scoped styles, event handling, two-way data-binding, updating state, etc

--- src/App.js
+++ src/App.js
@@ -1,27 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      {
+        name: 'Vikas Kumar',
+        age: 25
+      },
+      {
+        name: 'Ranjini Dubey',
+        age: 23
+      },
+      {
+        name: 'Ravan',
+        age: 'too long'
+      },
+      {
+        name: 'Upasana',
+        age: 1
+      }
+    ]
+  };
+  switchNameHandler = newName => {
+    // console.log('Was clicked!');
+    // this.state.people[0].name = 'dzen'; // WRONG WAY
+    this.setState({
+      people: [
+        {
+          name: 'Vivek Kumar',
+          age: 25
+        },
+        {
+          name: 'Ranjini Dubey',
+          age: 23
+        },
+        {
+          name: newName,
+          age: 'too long'
+        },
+        {
+          name: 'Upasana',
+          age: 1.5
+        }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      people: [
+        {
+          name: event.target.value,
+          age: 25
+        },
+        {
+          name: 'Ranjini Dubey',
+          age: 23
+        },
+        {
+          name: 'Ravan',
+          age: 'too long'
+        },
+        {
+          name: 'Upasana',
+          age: 1
+        }
+      ]
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <h2>Vikas Kumar</h2>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'RAVAN')}
+        >
+          Switch Name
+        </button>
+        <Person
+          name={this.state.people[0].name}
+          age={this.state.people[0].age}
+          changed={this.nameChangedHandler}
+        />
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, 'RAVAN!!!')}
+        />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>
+          My hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.people[3].name}
+          age={this.state.people[3].age}
+        />
       </div>
     );
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, "Hi, I'm a React App")
+    // );
   }
 }
 
